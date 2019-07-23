3c37055ba1df32c6ef6cff066fb2e32df926f55d
jsh901220 <joe1220@daum.net>
2019-07-23 17:14:54 +0900

    Passport JWT

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,33 @@
-import uuidv4  from 'uuid/v4'
+require("dotenv").config();
+
+import rand from 'rand-token'
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport"
+import jwt from 'jsonwebtoken'
 
 export const generateSecret = () => {
-  return uuidv4()
-}
+  return rand.generate(6, '0123456789')
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "jsh901220@gmail.com",
+    to: adress,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`
+  };
+  return sendMail(email);
+};
+
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET)
