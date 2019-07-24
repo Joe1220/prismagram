bdfaf495077b4ad64d796cc046dfa82f0bcec319
jsh901220 <joe1220@daum.net>
2019-07-24 19:59:27 +0900

    seeFullPost Refactoring

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,34 @@
-export const COMMNET_FRAGMENT = `
-  fragment CommentsParts on Commnet {
+export const USER_FRAGMENT = `
+    id
+    username
+`;
+
+export const COMMENT_FRAGMENT = `
     id
     text
     user {
-      username
+        ${USER_FRAGMENT}
     }
-  }
-`
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+    }
+`;
