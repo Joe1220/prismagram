a37be66ff80a1ecb81836da487539a6be841be86
jsh901220 <joe1220@daum.net>
2019-07-28 21:18:20 +0900

    fix Can delete USER([모델변경)

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,11 @@ import { prisma } from "../../../../generated/prisma-client";
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = prisma.$exists.user({ username })
+      console.log('check exist: ', exists)
+      if(exists) {
+        throw Error("This username is already taken")
+      }
       await prisma.createUser({
         username,
         email,
